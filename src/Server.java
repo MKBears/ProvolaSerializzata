4d7276da221024505e2a8eaa6940d77e879455cc
Marco Orsi <100961566+MKBears@users.noreply.github.com>
2022-05-06 16:26:10 +0200

    Sistema server

--- src/Server.java
+++ src/Server.java
@@ -20,15 +20,15 @@ public class Server extends Thread{
 
         try { /////////////////////////////SBAGLIATO IL SERVERRRRRRRRRRR
             sSocket = new ServerSocket();
-            ip_mio=new InetSocketAddress(Inet4Address.getLocalHost(),4500);
+            ip_mio=new InetSocketAddress(Inet4Address.getLocalHost(),4898);
             sSocket.bind(ip_mio); //qualcosa di sbagliato qui
             System.out.println("Server: pronto all'indirizzo ip  "+sSocket.getLocalSocketAddress());
-            client = sSocket.accept();
-            System.out.println("Server: connesso, aspetto notizie del client");
-            in = new ObjectInputStream(client.getInputStream());
+            ///client = sSocket.accept();
+            //System.out.println("Server: connesso, aspetto notizie del client");
+            //in = new ObjectInputStream(client.getInputStream());
             byte[] buf = new byte[256];
             packet = new DatagramPacket(buf, buf.length);
-            packet=(DatagramPacket) in.readObject();
+            packet = new DatagramPacket(buf, 0, 256);
             String received = new String(packet.getData());
             //ip=(InetSocketAddress) in.readObject();
             System.out.println("Client: ricevuto ip: "+received);
@@ -41,7 +41,7 @@ public class Server extends Thread{
             a = (AssistantCard)in.readObject();
             System.out.println("Server: carta ricevuta: "+a.getValue()+", "+ a.getMNSteps());*/
             sSocket.close();
-        } catch (IOException | ClassNotFoundException e) {
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
