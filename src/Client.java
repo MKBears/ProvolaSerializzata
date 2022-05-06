272ab2fe972a956168e4a8ef3f56cb57f97d3483
Marco Orsi <100961566+MKBears@users.noreply.github.com>
2022-05-06 09:55:50 +0200

    Client ultra mega powah che manda roba in broadcast

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,9 @@
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.net.Inet4Address;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 import java.net.Socket;
 
 public class Client extends Thread{
@@ -9,13 +13,22 @@ public class Client extends Thread{
         Student s;
         AssistantCard a;
         ObjectOutputStream out;
+        ObjectInputStream in;
 
         try {
-            socket = new Socket("10.169.151.169", 4898);
-            System.out.println("Client: connesso");
+            socket = new Socket("255.255.255.255", 4898);
             out = new ObjectOutputStream(socket.getOutputStream());
+            in = new ObjectInputStream(socket.getInputStream());
             s = new Student(StudentTypes.DRAGON);
             a = new AssistantCard(6, 3);
+            out.writeObject(new InetSocketAddress(Inet4Address.getLocalHost(), socket.getPort()));
+
+            try {
+                socket = new Socket((Proxy)in.readObject());
+                System.out.println("Client: connesso");
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
             out.writeObject(s);
             System.out.println("Client: studente inviato");
             out.writeObject(a);
